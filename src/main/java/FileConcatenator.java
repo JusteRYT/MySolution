372f375eeecdccf226e390ccb373a5928621875f
Sergey <tohappyoneguy@gmail.com>
2024-10-31 19:08:19 +0300

    Написал логику слияние и сортировку файлов

--- src/main/java/FileConcatenator.java
+++ src/main/java/FileConcatenator.java
@@ -1,4 +1,17 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.List;
+
 //Конкатенация файлов на основе упорядоченного списка
 public class FileConcatenator {
-
+    public void concatenateFiles(List<File> sortedFiles, String outputFilePath) throws IOException{
+        try (FileWriter writer = new FileWriter(outputFilePath)){
+            for(File file : sortedFiles){
+                writer.write(new String(Files.readAllBytes(file.toPath())));
+                writer.write(System.lineSeparator());
+            }
+        }
+    }
 }
