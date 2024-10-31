8787394744a60828248e7abce1ab01702724f465
Sergey <tohappyoneguy@gmail.com>
2024-10-31 19:12:20 +0300

    Добавление теста для класса FileConcatenator

---
+++ src/test/java/FileConcatenatorTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class FileConcatenatorTest {
+
+    @Test
+    public void testConcatenateFiles() throws Exception{
+        Path tempDir = Files.createTempDirectory()
+    }
+}
