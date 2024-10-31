3d0d4043e90e85feaac9bad8a0b51359200074e4
Sergey <tohappyoneguy@gmail.com>
2024-10-31 19:38:33 +0300

    Логика отработки программы

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,28 @@
+import java.io.File;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        String directoryPath = System.getProperty("user.dir");
+        String outputFilePath = "output.txt";
 
+        try {
+            //Поиск всех файлов в данной директории
+            DependencyManager manager = new DependencyManager();
+            List<File> files = manager.findTextFiles(new File(directoryPath));
+
+            //Анализ и сортировка
+            manager.analyzeDependencies(files);
+            List<File> sortedFiles = manager.sortFiles();
+
+            //Конкатенация файлов
+            FileConcatenator concatenator = new FileConcatenator();
+            concatenator.concatenateFiles(sortedFiles, outputFilePath);
+
+            System.out.println("Files concatenated successfully into" + outputFilePath);
+
+        } catch (Exception e){
+            System.err.println("Error: " + e.getMessage());
+        }
     }
 }
